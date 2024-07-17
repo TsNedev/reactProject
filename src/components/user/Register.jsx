c804a7d67ea244cc9d6396d35dc19fb7f0843ee2
TsNedev <tsvetannedev360@gmail.com>
2024-07-17 09:14:15 +0300

    addingRegisterForm

--- src/components/user/Register.jsx
+++ src/components/user/Register.jsx
@@ -1,5 +1,23 @@
+import './Register.css'
+
 export default function Register(){
     return(
-        <p>Register</p>
+        <div class="wrapper">
+        <section className="registerForm  ">
+        <form action="" method="post">
+            <input type="text" placeholder="First Name"/>
+            <input type="text" placeholder="Last Name"/>
+            <input type="text" placeholder="User Name"/>
+            <input type="text" placeholder="Image"/>
+            <input type="number" placeholder="Age"/>
+            <input type="email" placeholder="Email address"/>
+            <input type="password" placeholder="Password"/>
+            <input type="password" placeholder="Repeat Password"/>
+
+              <button type="submit" className="grow">Register</button>
+              <a className="link grow">Have Account? Login </a>
+        </form>
+    </section>
+    </div>
     )
 }
