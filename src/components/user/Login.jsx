017ee7a68b8a0fdae4d778e8092e655461b82476
TsNedev <tsvetannedev360@gmail.com>
2024-07-21 19:35:57 +0300

    adding custom hook

--- src/components/user/Login.jsx
+++ src/components/user/Login.jsx
@@ -1,15 +1,21 @@
-
+import useForm from '../../hooks/useForm'
 import './Login.css'
 
 
 export default function Login(){
+
+    const {values,onChange,onSubmit}=useForm(
+        {username:'',
+            password:''
+        }
+    ); 
     return (
         <div className="wrapper">
         <section className="loginForm  ">
         <h1 className="hederLogin">Login</h1>
-        <form action="">
-            <input type="text" placeholder="Username"/>
-            <input type="password" placeholder="Password"/>
+        <form action="" onSubmit={onSubmit}>
+            <input type="text" placeholder="Username" id='username' name='username' value={values.username} onChange={onChange}/>
+            <input type="password" placeholder="Password" id='password' name='password' value={values.password} onChange={onChange} />
             <button className="grow">Login</button>
 
             <a className="link grow " href="#">Not a Member? Register</a>
---
+++ src/hooks/useForm.js
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+export default function useForm(submitHandlers,initialValues){
+
+    const [values , setValues] = useState(initialValues); 
+
+
+
+   const onChange = (e) => { 
+ 
+      setValues(state =>({
+        ...state,
+        [e.target.name]: e.target.value
+      }))
+     }
+
+ 
+     const onSubmit =(e) =>{
+        e.preventDefault(); 
+
+        submitHandlers(values);
+     }
+
+     return{
+        values,
+        onChange,
+        onSubmit
+     }
+
+}
